9c31b1dc2916fa86f8f3248c53ce82b28ea94311
RMA101 <rhenanderson@Rhens-MacBook-Pro.local>
2016-12-12 11:37:22 -0700

    Actions and Outlets added within Interface Builder

---
+++ GameCollector/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by Rhen Anderson on 12/12/16.
+//  Copyright © 2016 Rhen Anderson. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var gameImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+}
